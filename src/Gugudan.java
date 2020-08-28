03b5e4432ecceb7694189aa5775359e51ce0ecfc
jina2010 <xinatte@gmail.com>
2020-08-28 17:02:09 +0900

    4, 5단 구현 - 값 입력 및 변수

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,32 +1,70 @@
+import java.util.Scanner;
 
 public class Gugudan {
 	public static void main(String[] args) {
 		// 2단
-		System.out.println("2단");
-		System.out.println(2 * 1);
-		System.out.println(2 * 2);
-		System.out.println(2 * 3);
-		System.out.println(2 * 4);
-		System.out.println(2 * 5);
-		System.out.println(2 * 6);
-		System.out.println(2 * 7);
-		System.out.println(2 * 8);
-		System.out.println(2 * 9);
+//		System.out.println("2단");
+//		System.out.println(2 * 1);
+//		System.out.println(2 * 2);
+//		System.out.println(2 * 3);
+//		System.out.println(2 * 4);
+//		System.out.println(2 * 5);
+//		System.out.println(2 * 6);
+//		System.out.println(2 * 7);
+//		System.out.println(2 * 8);
+//		System.out.println(2 * 9);
 		
 		// 라인 복사 단축키 : ctrl + alt + 아래/위 화살표
 		// 라인 이동 단축키 : alt + 아래/위 화살표
 		
 		// 3단
-		System.out.println("3단");
-		System.out.println(3 * 1);
-		System.out.println(3 * 2);
-		System.out.println(3 * 3);
-		System.out.println(3 * 4);
-		System.out.println(3 * 5);
-		System.out.println(3 * 6);
-		System.out.println(3 * 7);
-		System.out.println(3 * 8);
-		System.out.println(3 * 9);	
+//		System.out.println("3단");
+//		System.out.println(3 * 1);
+//		System.out.println(3 * 2);
+//		System.out.println(3 * 3);
+//		System.out.println(3 * 4);
+//		System.out.println(3 * 5);
+//		System.out.println(3 * 6);
+//		System.out.println(3 * 7);
+//		System.out.println(3 * 8);
+//		System.out.println(3 * 9);	
 		
+		// 주석처리 : 드래그 + ctrl + /
+		
+		// 4단
+		System.out.println("4단");
+		int result = 4 * 1;	// result 라는 int형 변수 생성
+		System.out.println(result);
+		result = 4 * 2;
+		System.out.println(result);
+		result = 4 * 3;
+		System.out.println(result);
+		result = 4 * 4;
+		System.out.println(result);
+		result = 4 * 5;
+		System.out.println(result);
+		result = 4 * 6;
+		System.out.println(result);
+		result = 4 * 7;
+		System.out.println(result);
+		result = 4 * 8;
+		System.out.println(result);
+		result = 4 * 9;
+		System.out.println(result);
+		
+		
+		System.out.println("구구단 중 출력할 단은? : ");
+		Scanner scanner = new Scanner(System.in); // 값을 입력 받음
+		int number = scanner.nextInt(); // 입력 받을 값은 int형
+		System.out.println(number * 1);
+		System.out.println(number * 2);
+		System.out.println(number * 3);
+		System.out.println(number * 4);
+		System.out.println(number * 5);
+		System.out.println(number * 6);
+		System.out.println(number * 7);
+		System.out.println(number * 8);
+		System.out.println(number * 9);
+	
 	}
 }
