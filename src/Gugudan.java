b701ce3c12a4d6347831df8eae374e720a2fa1f4
jina2010 <xinatte@gmail.com>
2020-08-28 18:14:45 +0900

    배열로 구구단 다시 구현

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,103 +1,17 @@
-import java.util.Scanner;
 
 public class Gugudan {
 	public static void main(String[] args) {
-		
-		// 8, 9단
-		System.out.println("구구단 중 출력할 단은? : ");
-		Scanner scanner = new Scanner(System.in); 
-		int number = scanner.nextInt();
-		System.out.println("사용자가 입력한 값 : " + number);
-		
-		if(number < 2) {
-			System.out.println("값을 잘못 입력했습니다.");
-		}
-		else if(number > 9) {
-			System.out.println("값을 잘못 입력했습니다.");			
-		}
-		else {
-			for(int i = 1; i < 10; i++) {
-				System.out.println(number * i);
+		int[] result = new int[100]; // 100개의 값을 받는 int형 배열 생성 
+		int num=0; // 배열의 순서를 담을 변수
+		for(int i=2; i<10; i++) { 
+			for(int j=1; j<10; j++) {
+				result[num] = i * j;
+				num++;		
 			}
 		}
 		
-		
-		
-		// 6단
-//		int i = 1;
-//		while(i < 10) {
-//			System.out.println(6 * i);
-//			i = i + 1;
-//		}
-		
-		// 7단
-//		for(int j = 1; j < 10; j++) {
-//			System.out.println(7 * j);
-//		}
-		
-		
-		// 2단
-//		System.out.println("2단");
-//		System.out.println(2 * 1);
-//		System.out.println(2 * 2);
-//		System.out.println(2 * 3);
-//		System.out.println(2 * 4);
-//		System.out.println(2 * 5);
-//		System.out.println(2 * 6);
-//		System.out.println(2 * 7);
-//		System.out.println(2 * 8);
-//		System.out.println(2 * 9);
-		
-		// 라인 복사 단축키 : ctrl + alt + 아래/위 화살표
-		// 라인 이동 단축키 : alt + 아래/위 화살표
-		
-		// 3단
-//		System.out.println("3단");
-//		System.out.println(3 * 1);
-//		System.out.println(3 * 2);
-//		System.out.println(3 * 3);
-//		System.out.println(3 * 4);
-//		System.out.println(3 * 5);
-//		System.out.println(3 * 6);
-//		System.out.println(3 * 7);
-//		System.out.println(3 * 8);
-//		System.out.println(3 * 9);	
-		
-		// 주석처리 : 드래그 + ctrl + /
-		
-		// 4단
-//		System.out.println("4단");
-//		int result = 4 * 1;	// result 라는 int형 변수 생성
-//		System.out.println(result);
-//		result = 4 * 2;
-//		System.out.println(result);
-//		result = 4 * 3;
-//		System.out.println(result);
-//		result = 4 * 4;
-//		System.out.println(result);
-//		result = 4 * 5;
-//		System.out.println(result);
-//		result = 4 * 6;
-//		System.out.println(result);
-//		result = 4 * 7;
-//		System.out.println(result);
-//		result = 4 * 8;
-//		System.out.println(result);
-//		result = 4 * 9;
-//		System.out.println(result);
-		
-		
-//		System.out.println("구구단 중 출력할 단은? : ");
-//		Scanner scanner = new Scanner(System.in); // 값을 입력 받음
-//		int number = scanner.nextInt(); // 입력 받을 값은 int형
-//		System.out.println(number * 1);
-//		System.out.println(number * 2);
-//		System.out.println(number * 3);
-//		System.out.println(number * 4);
-//		System.out.println(number * 5);
-//		System.out.println(number * 6);
-//		System.out.println(number * 7);
-//		System.out.println(number * 8);
-//		System.out.println(number * 9);		
+		for(int i=0; i < num; i++) {
+			System.out.println(result[i]);
+		}
 	}
 }
