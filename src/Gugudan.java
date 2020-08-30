34148eda9738ab279358489e5281d0d98c157c64
jina2010 <xinatte@gmail.com>
2020-08-30 13:33:39 +0900

    메소드를 활용해 구구단 다시 구현

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,17 +1,25 @@
 
 public class Gugudan {
-	public static void main(String[] args) {
-		int[] result = new int[100]; // 100개의 값을 받는 int형 배열 생성 
-		int num=0; // 배열의 순서를 담을 변수
-		for(int i=2; i<10; i++) { 
-			for(int j=1; j<10; j++) {
-				result[num] = i * j;
-				num++;		
-			}
+	public static int[] calculate(int times) {
+		int[] result = new int[9];
+		for(int i=0; i<result.length; i++) {
+			result[i] = times * (i + 1);
 		}
-		
-		for(int i=0; i < num; i++) {
+		return result;
+	}
+	
+	public static void print(int[] result) {
+		for(int i=0; i < result.length; i++) {
 			System.out.println(result[i]);
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		for(int i=2; i<10; i++) {
+			int[] result = calculate(i);
+			print(result);
 		}
 	}
 }
